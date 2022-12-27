ce432db8296a987df66994f390d925b33cf3203a
Leemyeongjun98 <mj980401@gmail.com>
2022-12-27 20:21:53 +0900

    22-12-27

--- js/main.js
+++ js/main.js
@@ -40,3 +40,22 @@ window.addEventListener('scroll', _.throttle(function () {
 
     }
 }, 300));
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+    //gsap.to(요소, 지속시간, 옵션);
+    gsap.to(fadeEl, 1, {
+        delay: (index + 1) * .7, //0.7초, 1.4, 2.1, 2.8
+        opacity: 1
+    });
+});
+
+
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+    direction:'vertical',
+    autoplay: true,
+    loop: true, 
+});
+
